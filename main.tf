61e18b47f778051d52b533c5d9a549229d368e85
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-09 16:02:29 +0100

    dfvsfdgsdffgssdfg

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.11"
+      version = "4.0.0"
     }
   }
   required_version = ">= 0.14.9"
