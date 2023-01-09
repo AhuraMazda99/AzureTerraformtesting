9118264c106a2be3669423e6ef5610e916b35c3e
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-09 15:41:29 +0100

    test

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "rg" {
 
 module "vnet-Main" {
   source  = "Azure/vnet/azurerm"
-  version = "4.0.0"
+  version = "~> 4.0.0"
   # insert the 3 required variables here
   resource_group_name = azurerm_resource_group.rg
 
