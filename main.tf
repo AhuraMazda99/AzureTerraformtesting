aabea3a4f88885252ac0ad13d49832dd58bee5f2
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-10 08:27:57 +0100

    bdfsgdfsg

--- main.tf
+++ main.tf
@@ -16,16 +16,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "jizan-terraform"
   location = var.region
 }
-
-module "vnet" {
-  source  = "Azure/vnet/azurerm"
-  version = "4.0.0"
-  # insert the 3 required variables here
-  use_for_each = false 
-  vnet_location = var.region
-  resource_group_name = azurerm_resource_group.rg
-
-
-
-
-}
