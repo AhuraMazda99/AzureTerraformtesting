cdf31e335c864773c80a57c8ab943b5f3ec2bf1e
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-10 08:33:22 +0100

    dfsgdfsgdfs

--- main.tf
+++ main.tf
@@ -16,3 +16,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "jizan-terraform"
   location = var.region
 }
+
+module "network" {
+  source  = "Azure/network/azurerm"
+  version = "5.0.0"
+  # insert the 2 required variables here
+  resource_group_name = azurerm_resource_group.rg
+  use_for_each = false
+  
+}
