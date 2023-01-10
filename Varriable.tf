043f05d0e723cbacf45f3b64535b4530dd48e027
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-10 09:19:39 +0100

    fsadfsda

--- Varriable.tf
+++ Varriable.tf
@@ -13,7 +13,7 @@ default = "10.50.0.0/16"
 variable "subnet1" {
 description = "subnet range for network"
 type = string
-default = "10.50.20.0/16"
+default = "10.50.0.0/16"
 }
 
 variable "subnet2" {
