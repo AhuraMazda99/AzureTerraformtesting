dd73ed3592ce2574fe6e0bb9553c01857766cb41
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-11 12:57:32 +0100

    dfgsdfgsdfgsd

--- main.tf
+++ main.tf
@@ -63,6 +63,19 @@ resource "azurerm_key_vault" "KY" {
   soft_delete_retention_days = 7
   purge_protection_enabled = false
   sku_name = "standard"
+    access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "get",
+    ]
+    secret_permissions = [
+      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
+    ]
+    storage_permissions = [
+      "get",
+    ]
+  }
 }
 
 resource "azurerm_key_vault_key" "test" {
