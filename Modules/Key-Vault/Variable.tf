21dbf0870db61b18b799b761e0a1ab4c40204529
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-12 14:07:26 +0100

    testin modules

---
+++ Modules/Key-Vault/Variable.tf
@@ -0,0 +1,16 @@
+variable "Key_vault_name" {
+  description = "The name of the key Vault"
+  type = string
+}
+variable "sku" {
+  description = "Sku premium or standard"
+  type = string
+}
+variable "tenent_id" {
+  description = "tenent_id"
+  type = string
+}
+variable "object_id" {
+  description = "Object id"
+  type = string
+}
