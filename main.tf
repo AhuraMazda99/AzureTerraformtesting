0eed2cf49b4b23e3b20d853ff9fa2671e5e784ff
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-13 11:10:44 +0100

    testing Modules

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "azurerm_network_interface_security_group_association" "Vnet-Main-NSG" 
 }
 
 module "Key_vault" {
-  source = "github.com/azure/Modules/Key-Vault"
+  source = "github.com/azure/Modules/"
   Key_vault_name = "Jizan-keyvaultname"
   Sku = "Standard"
   tenent_id = data.azurerm_client_config.tenant_id
