f893541462749e39ad918d2210162616209552f4
Benjamin Jizan <Jizan@nordictrustee.com>
2023-01-13 14:46:03 +0100

    dsfdsfdsfsd

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ module "Key_vault" {
   sku = "standard"
   tenent_id = data.azurerm_client_config.current.tenant_id
   object_id = data.azurerm_client_config.current.object_id
-  rg = output.resource_group_name
+  rg = azurerm_resource_group.rg.name
   location = var.region
 }
 
