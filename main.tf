e4978eaf94f04d706c5d49025efaf8b3c6056264
Benjamin Jizan <Jizan@nordictrustee.com>
2023-02-13 10:11:01 +0100

    fixing vm name error

--- main.tf
+++ main.tf
@@ -188,7 +188,7 @@ module "Secret_adminpassword" {
 #VM
 module "vm_test_Windows_2016" {
   source = "./Modules/VM"
-  name = "vm_test"
+  name = "vmtest"
   resource_group_name = azurerm_resource_group.rg.name
   admin_username = "adminNT"
   network_interface_id = [azurerm_network_interface.Netinterface.id]
