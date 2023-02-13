6b02444c4493342f188b82bd361306ca9dd77085
Benjamin Jizan <Jizan@nordictrustee.com>
2023-02-13 09:06:58 +0100

    modulizing of vm

---
+++ Modules/VM/Variable.tf
@@ -0,0 +1,16 @@
+variable "name" {
+  description = "Name of VM"
+  type = string
+}
+variable "admin_username" {
+  description = "username of admin user"
+  type = string
+}
+variable "network_interface_id" {
+  description = "network interface id"
+  type = string
+}
+variable "resource_group_name" {
+  description = "Resource group name"
+  type = string
+}
---
+++ Modules/VM/outputs.tf
@@ -0,0 +1,7 @@
+output "vm_test_Windows_2016_id" {
+  value = azurerm_windows_virtual_machine.vm_test_Windows_2016.id
+}
+
+output "vm_test_Windows_2016" {
+  value = azurerm_windows_virtual_machine.vm_test_Windows_2016
+}
